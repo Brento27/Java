927ab3d2f7842e186b0adadb3da1289232a50ddd
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-07-29 11:24:05 +0200

    Finished chapter 5-6

--- java_Advanced/ClassWork/D84838GC10_labs/labs/10-LambdaOperations/practices/practice1/SalesTxn10-01Prac/src/com/example/lambda/PeekTest.java
+++ java_Advanced/ClassWork/D84838GC10_labs/labs/10-LambdaOperations/practices/practice1/SalesTxn10-01Prac/src/com/example/lambda/PeekTest.java
@@ -8,19 +8,24 @@ import java.util.function.Consumer;
  * @author oracle
  */
 public class PeekTest {
-    
-    public static void main(String[] args) {
-    
-    List<SalesTxn> tList = SalesTxn.createTxnList();
-    Consumer<SalesTxn> taxReport = 
-      t -> System.out.printf("Id: " + t.getTxnId() 
-        + " Buyer: " + t.getBuyerName() + 
-        " Txn amt: $%,9.2f ", t.getTransactionTotal());
-    
-    // Write report using peek
-    System.out.println("=== Widget Pro Sales Tax in CA ===");
 
+    public static void main(String[] args) {
+        List<SalesTxn> tList = SalesTxn.createTxnList();
+        Consumer<SalesTxn> taxReport
+                = t -> System.out.printf("Id: " + t.getTxnId()
+                        + " Buyer: " + t.getBuyerName()
+                        + " Txn amt: $%,9.2f ", t.getTransactionTotal());
         
         
+        System.out.println("=== Widget Pro Sales Tax in CA ===");
+
+        // Calculate sales tax for CA Transactions
+        tList.stream()
+                .filter(t -> t.getState().equals(State.CA))
+                .filter(t -> t.getProduct().equals("Widget Pro"))
+                .peek(taxReport)
+                .map(t -> t.getTransactionTotal() * TaxRate.byState(t.getState()))
+                .forEach(amt -> System.out.printf("Txn tax: $%,9.2f%n", amt));
+
     }
 }
---
+++ java_Advanced/ClassWork/D84838GC10_labs/labs/11-Exceptions/practices/practice1/CatchingExceptions11-01/src/catchingexceptions11/pkg01/CatchingExceptions1101.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package catchingexceptions11.pkg01;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Brent
+ */
+public class CatchingExceptions1101 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println("Reading from file:" + args[0]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("No file specified, quitting!");
+            System.exit(1);
+        }
+
+        try ( BufferedReader b
+                = new BufferedReader(new FileReader(args[0]));) {
+            String s = null;
+            while ((s = b.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found:" + args[0]);
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("Error reading file:" + e.getMessage());
+            System.exit(1);
+        }
+
+    }
+
+}
