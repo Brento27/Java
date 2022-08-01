9b4309ad6055989b2a1c09b9b5d3b41f2076f13a
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-08-01 14:04:48 +0200

    Finished dummies_3_6

---
+++ java_Beginner/classwork/dummies_book_practice/Book_03/javaForDummiesBook3/src/Chapter4/Summary.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chapter4;
+
+/**
+ *
+ * @author Brent
+ */
+public class Summary {
+    public static void main(String[] args) {
+        System.out.println("Subclasses");
+        System.out.println("Overiding methods");
+        System.out.println("Protecting members");
+        System.out.println("this and super");
+        System.out.println("constructors");
+        System.out.println("final");
+        System.out.println("up and down casting");
+        System.out.println("instaceof");
+        System.out.println("custom exceptions");
+    }
+}
